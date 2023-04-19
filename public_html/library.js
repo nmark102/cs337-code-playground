94d3f322a6e0e5ab59435e9af655418442c63d65
8ng31 <125998693+8ng31@users.noreply.github.com>
2023-04-18 23:56:00 -0700

    Delete library.js

--- public_html/library.js
+++
@@ -1,9 +0,0 @@
-/*
- * Author(s): Angel Benavides, Mark Nguyen, Nishant Athawale                       
- * Class: CS 337, Spring 2023                                                      
- * File: library.js                                                                
- * Assignment: Final Project - LeetCode Clone                                      
- * Purpose: Implement functions for library.html that will make requests to
- * server or implement function.
- */
-
