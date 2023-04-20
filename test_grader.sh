11a08b68e151e58149904c630a3d093f8c4365a2
Mark Nguyen <trimarknguyen@gmail.com>
2023-04-19 23:23:53 -0700

    More complete autograder tester

--- test_grader.sh
+++ test_grader.sh
@@ -1,3 +1,7 @@
+cd src/grader/
+./build_grader.sh
+cd ../../
+
 echo "Testing grader"
 echo "=============="
 
@@ -10,12 +14,10 @@ echo $(./bin/grader -l python3 -t 2 -T 1-add-two-numbers -s 1)
 
 echo "Testcase 2: Language: Java. Expected status 1 (compile error)."
 
-echo $?
+echo $(./bin/grader -l java -T 1-add-two-numbers -s 2)
 
 echo "Testcase 3: Language: C. Expected status 4 (time limit exceeded)."
-
-echo $?
+echo $(./bin/grader -l c -T 1-add-two-numbers -s 3)
 
 echo "Testcase 4: Language: Python. Expected status 2 (wrong answer)."
-
-echo $?
+echo $(./bin/grader -l python3 -t 2 -T 1-add-two-numbers -s 4)
