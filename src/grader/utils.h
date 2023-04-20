37ec268348828f6aa9a816855c1e0e03e4f90be1
Mark Nguyen <trimarknguyen@gmail.com>
2023-04-19 22:20:17 -0700

    More configs added

--- src/grader/utils.h
+++ src/grader/utils.h
@@ -3,12 +3,31 @@
 
 using namespace std;
 
-// Some default constants
-const string SUBMISSIONS_BASE_DIR = "~/submissions/";
+// Default verdicts
+const int GRADER_CRASHED = -1;
+const int ACCEPTED = 0;
+const int COMPILER_ERROR = 1;
+const int RUNTIME_ERROR = 2;
+const int WRONG_ANSWER = 3;
+const int TIME_LIMIT_EXCEEDED = 4;
+const int MEMORY_LIMIT_EXCEEDED = 5;
 
-const string DEFAULT_TIME_LIMIT = " 1s ";
-const string DEFAULT_MEM_LIMIT = "64m";
-const string DEFAULT_GCC_ARGS = " -O2 -fno-omit-frame-pointer -fsanitize=address ";
-const string DEFAULT_GPP_ARGS = " -O2 -fno-omit-frame-pointer -fsanitize=address ";
+// Some default system return codes
+const int TIMEOUT_EXIT_CODE = 124;
+const int MEM_LIMIT_EXIT_CODE = 137;
 
-void readlines(vector<string>& dest, const string filename);
+// Default compiler arguments
+const string DEFAULT_GCC_ARGS = "-O2 -fno-omit-frame-pointer -fsanitize=address ";
+const string DEFAULT_GPP_ARGS = "-O2 -fno-omit-frame-pointer -fsanitize=address ";
+const string DEFAULT_JAVA_ARGS = "";
+
+// Default runtime arguments
+const string DEFAULT_TIME_LIMIT = "1s ";
+const string DEFAULT_MEM_LIMIT = "64m ";
+
+// Default file paths
+const string SUBMISSIONS_BASE_DIR = "./submissions/";
+const string TESTCASES_BASE_DIR = "./testcases/";
+
+// Function headers
+void readlines(vector<string>* dest, const string filename);
